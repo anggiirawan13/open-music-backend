264bd056f1f355e805374e9e63e5bb9b82b89583
AnggiIrawann <anggi.irawan@nexsoft.co.id>
2025-01-27 17:31:00 +0700

    feat: init redis, rabbit, and new column and table

---
+++ migrations/1737973697692_create-table-user-album-likes.js
@@ -0,0 +1,34 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('user_album_likes', {
+    id: {
+      type: 'VARCHAR(32)',
+      primaryKey: true,
+    },
+    album_id: {
+      type: 'VARCHAR(22)',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(21)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('user_album_likes', 'fk_user_album_likes.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE');
+
+  pgm.addConstraint('user_album_likes', 'fk_user_album_likes.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('user_album_likes');
+};
